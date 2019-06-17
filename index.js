52ae1ec7df47b54265cc49bef4cab12a4012f993
Robin Millette <robin@millette.info>
2019-06-17 00:33:37 -0400

    Implements #30: support more abstract-leveldown compliant stores.

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const level = require('level')
+const levelup = require('levelup')
+const leveldown = require('leveldown')
+const encode = require('encoding-down')
+
+// const level = require('level')
+
+// mostly from level-packager
+const levelMore = (location, options) => {
+  if (typeof options !== 'object' || options === null)  options = {}
+  const store = options.store || leveldown
+  ;[ 'destroy', 'repair' ].forEach(function (m) {
+    if (typeof store[m] === 'function') {
+      levelMore[m] = () => store[m].apply(store, arguments)
+    }
+  })
+
+  return levelup(encode(store(location), options), options)
+}
+
+
+levelMore.errors = levelup.errors
 
 function levelPlugin (fastify, opts, next) {
-  if (!opts.name) {
-    return next(new Error('Missing database name'))
-  }
+  console.log('OPTS', opts)
+  if (!opts.name && (!opts.options || !opts.options.store)) return next(new Error('Missing database name'))
   opts.options = opts.options || {}
 
   fastify
-    .decorate('level', level(opts.name, opts.options))
+    .decorate('level', levelMore(opts.name, opts.options))
     .addHook('onClose', close)
 
   next()
