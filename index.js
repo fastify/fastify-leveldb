20d4b3dfee93471211562ae9b95a08c60194b70c
Robin Millette <robin@millette.info>
2019-06-18 11:27:33 -0400

    Support more abstract-leveldown compliant stores (#31)
    * Replace level with its parts (levelup, leveldown, encoding-down).
    
    * Implements #30: support more abstract-leveldown compliant stores.
    
    * Make tests pass (standard --fix).
    
    * Cleanup...
    
    * Add memdown dev dep.
    
    * Test memdown store.
    
    * Add memdown example.
    
    * Instantiate fastify properly (see also #29 issue).

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const level = require('level')
+const levelup = require('levelup')
+const leveldown = require('leveldown')
+const encode = require('encoding-down')
+
+// mostly from level-packager
+const levelMore = (location, options) => {
+  if (typeof options !== 'object' || options === null) options = {}
+  const store = options.store || leveldown
+  delete options.store
+  ;[ 'destroy', 'repair' ].forEach(function (m) {
+    if (typeof store[m] === 'function') {
+      levelMore[m] = () => store[m].apply(store, arguments)
+    }
+  })
+
+  return levelup(encode(store(location), options), options)
+}
+
+levelMore.errors = levelup.errors
 
 function levelPlugin (fastify, opts, next) {
-  if (!opts.name) {
+  if (!opts.name && (!opts.options || !opts.options.store)) {
     return next(new Error('Missing database name'))
   }
   opts.options = opts.options || {}
 
   fastify
-    .decorate('level', level(opts.name, opts.options))
+    .decorate('level', levelMore(opts.name, opts.options))
     .addHook('onClose', close)
 
   next()
--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const t = require('tap')
 const test = t.test
 const rimraf = require('rimraf')
 const Fastify = require('fastify')
+const memdown = require('memdown')
 const level = require('./')
 
 t.tearDown(() => {
@@ -33,6 +34,27 @@ test('level should support leveldb operations', t => {
   const fastify = Fastify()
   fastify
     .register(level, { name: 'test' })
+    .ready(err => {
+      t.error(err)
+      fastify.level.put('a', 'b', err => {
+        t.error(err)
+        fastify.level.get('a', (err, val) => {
+          t.error(err)
+          t.equal(val, 'b')
+          fastify.close(() => {
+            t.pass('unlock')
+          })
+        })
+      })
+    })
+})
+
+test('level should support other stores (memdown)', t => {
+  t.plan(5)
+
+  const fastify = Fastify()
+  fastify
+    .register(level, { options: { store: memdown } })
     .ready(err => {
       t.error(err)
       fastify.level.put('a', 'b', err => {
