d607f90522eeae6675d127b2a3221412f947ff7d
Brett Adams <Bre77@users.noreply.github.com>
2025-03-22 17:06:16 +1000

    Fix mistake
    Signed-off-by: Brett Adams <Bre77@users.noreply.github.com>

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -8,7 +8,7 @@ const app = fastify()
 app
   .register(fastifyLeveldb, {
     name: 'test',
-    path: '.local'
+    path: '.local',
     options: { valueEncoding: 'json' }
   })
   .after(async (_err) => {
